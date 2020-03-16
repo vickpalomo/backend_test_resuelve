17922969bfc69a15c86223611226354f2819fe4e
Victor Palomo <ing.victorpalomo@gmail.com>
2020-03-16 11:53:22 -0600

    Return the json with the total salary of each player

--- service_salary/salary_controller.js
+++ service_salary/salary_controller.js
@@ -4,6 +4,7 @@ const calculateSalary = (req, res) => {
   const teamDataToGetSalary = lib.teamDataToReplaceLevels(req.body)
   const percentageTeam = lib.getPercentageTeam(teamDataToGetSalary)
   const teamDataFullSalary = lib.getTotalSalary(teamDataToGetSalary, percentageTeam)
+  return res.status(200).json(teamDataFullSalary)
 }
 
 module.exports = {
