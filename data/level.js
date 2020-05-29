ebb3173e29ab52b719b52846c806dc42c1d13f3e
Victor Palomo <ing.victorpalomo@gmail.com>
2020-05-28 23:20:58 -0500

    Change the json structure of the levels by team, the keys are the teams and the values of these keys are the levels

--- data/level.js
+++ data/level.js
@@ -1,8 +1,16 @@
 const levels = {
-  A: 5,
-  B: 10,
-  C: 15,
-  Cuauh: 20
+  rojo: {
+    A: 5,
+    B: 10,
+    C: 15,
+    Cuauh: 20
+  },
+  azul: {
+    A: 5,
+    B: 10,
+    C: 15,
+    Cuauh: 20
+  }
 }
 
 module.exports = levels
